ae7c424c512f7219daea5282c2062aeb22bd16a0
hatak <maciek.hatak@gmail.com>
2016-09-05 21:49:29 +0200

    added first test for repository

--- app/src/main/java/com/hatak/benefit/repository/Repository.java
+++ app/src/main/java/com/hatak/benefit/repository/Repository.java
@@ -26,6 +26,7 @@ import cz.msebera.android.httpclient.entity.StringEntity;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import rx.Observable;
+import rx.Scheduler;
 import rx.Subscriber;
 
 /**
@@ -36,21 +37,31 @@ public class Repository {
     private static final String CARD_NUMBER = "CARD_NUMBER";
     private static final String NIK_NUMBER = "NIK_NUMBER";
     private static final String FORM_DATA = "AuthenticationMethod=UsernameAuthenticator&BackURL=%2Fsaldo&Username=CARD_NUMBER&Password=NIK_NUMBER&action_dologin.x=51&action_dologin.y=14";
-    private static final String URL = "https://www.mypremium.pl/Security/LoginForm";
+    private static final String BASE_URL = "https://www.mypremium.pl/";
+    private static final String SALDO_PATH = "Security/LoginForm";
     private static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
     public static final String KEY_ID = "id";
     public static final int TIMEOUT = 30 * 1000; // 30 sec
 
     private static Repository instance;
     private Context appContext;
+    private final String baseUrl;
 
-    public static void initialize(final Context context) {
+    public Repository(final String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public static void initialize(@NonNull final Context context){
+        initialize(context, BASE_URL);
+    }
+
+    public static void initialize(@NonNull final Context context, @NonNull String baseUrl) {
         if (instance == null) {
             RealmConfiguration config = new RealmConfiguration.Builder(context.getApplicationContext())
                     .deleteRealmIfMigrationNeeded()
                     .build();
             Realm.setDefaultConfiguration(config);
-            instance = new Repository();
+            instance = new Repository(baseUrl);
             instance.appContext = context.getApplicationContext();
         }
     }
@@ -102,7 +113,7 @@ public class Repository {
             try {
                 SyncHttpClient client = getUnsafeSyncHttpClient();
                 StringEntity form = new StringEntity(FORM_DATA.replace(CARD_NUMBER, cardNumber).replace(NIK_NUMBER, nikNumber));
-                client.post(appContext, URL, form, CONTENT_TYPE, new TextHttpResponseHandler() {
+                client.post(appContext, baseUrl + SALDO_PATH, form, CONTENT_TYPE, new TextHttpResponseHandler() {
                     @Override
                     public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                         subscriber.onError(throwable);
---
+++ app/src/test/java/com/hatak/benefit/repository/RepositoryTest.java
@@ -0,0 +1,53 @@
+package com.hatak.benefit.repository;
+
+import com.hatak.benefit.R;
+import com.hatak.benefit.saldo.Saldo;
+
+import org.apache.commons.io.IOUtils;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import okhttp3.HttpUrl;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import rx.Observable;
+
+/**
+ * Created by hatak on 05.09.16.
+ */
+
+@RunWith(RobolectricTestRunner.class)
+public class RepositoryTest {
+
+    @Test
+    public void testGetSaldo() throws IOException {
+
+        MockWebServer server = new MockWebServer();
+        // Schedule some responses.
+        final String response = IOUtils.toString(ClassLoader.getSystemResourceAsStream("response_ok.html"));
+        server.enqueue(new MockResponse().setBody(response));
+        // Start the server.
+        server.start();
+        // Ask the server for its URL. You'll need this to make HTTP requests.
+        HttpUrl baseUrl = server.url("/");
+        // Exercise your application code, which should make those HTTP requests.
+        // Responses are returned in the same order that they are enqueued.
+
+        Repository repository = new Repository(baseUrl.toString());
+        final Saldo saldo = repository.getSaldo("1234", "abcd")
+                .toBlocking()
+                .first();
+
+        assertTrue(saldo.getSaldo().contains("100,04"));
+        assertTrue(saldo.getTransactions().size() > 1);
+
+        server.shutdown();
+    }
+
+}
